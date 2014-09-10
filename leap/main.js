3a4c61a95b2830b750ab6de3a9c28890ab86699b
ap186061 <adrian.pomilio@teradata.com>
2014-09-10 16:33:25 -0400

    sphero leap battle continues

--- leap/main.js
+++ leap/main.js
@@ -1,35 +1,58 @@
+// LEAP Motion
+
 var Cylon = require('cylon');
 
 Cylon.robot({
-  connection: {
-    name: 'leapmotion',
-    adaptor: 'leapmotion',
-    port: '127.0.0.1:6437'
-  },
+  name: 'sample',
+  connections:[
+    {
+      name: 'leapmotion',
+      adaptor: 'leapmotion',
+      port: '127.0.0.1:6437'
+    },
+    { name: 'sphero',
+    adaptor: 'sphero',
+    port: '/dev/tty.Sphero-BPW-AMP-SPP' }
 
-  device: {
-    name: 'leapmotion',
-    driver: 'leapmotion'
-  },
+  ] ,
+  devices: [
+    {
+      name: 'leapmotion',
+      driver: 'leapmotion'
+    }
+  ],
+
 
   work: function(my) {
-
+    console.log(my);
     my.leapmotion.on('gesture', function(gesture) {
-      console.log(gesture.type);
+
       if(gesture.state !== 'update'){
-        console.log('=============Start==================');
-        console.log(gesture.state);
-        console.log(gesture.type);
-        console.log(gesture.speed);
-        console.log('==============Stop==================');
 
-          if (gesture.type=='circle' ){
+
+          if (gesture.type === 'circle' ){
               console.log('========= A CIRCLE ===========');
+              my.sphero.stop();
           }
 
-          if (gesture.type=='keytap' ){
+          if (gesture.type === 'keytap' ){
               console.log('========= A Keytap ===========');
+              my.sphero.stop();
           }
+
+          if (gesture.type === 'swipe'){
+            if(gesture.direction[1] >= 0){
+              console.log('========= right========= ');
+              my.sphero.roll(180, 0);
+            }else{
+              console.log('========= left========= ');
+              my.sphero.roll(180, 180);
+            }
+            console.log('========= Swipe ===========');
+
+          }
+
+
       }
 
     });
--- sphero/main.js
+++ sphero/main.js
@@ -4,7 +4,7 @@ var calibration = false;
 
 
 Cylon.robot({
-  connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/tty.Sphero-GYR-AMP-SPP' },
+  connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/tty.Sphero-BPW-AMP-SPP' },
   device: { name: 'sphero', driver: 'sphero' },
   checkCalibration: (function(_this) {
     return function(me) {
@@ -19,18 +19,18 @@ Cylon.robot({
     my.checkCalibration(my);
     my.sphero.setBackLED(255);
 
-    // console.log("forward");
-    //   my.sphero.roll(180, 0);
-    //   my.sphero.stop();
-    //   my.sphero.roll(180, 180);
+    console.log("forward");
+      my.sphero.roll(180, 0);
+      my.sphero.stop();
+      my.sphero.roll(180, 180);
 
-    // every((1).second(), function() {
-    //   my.sphero.setRGB(Math.floor(Math.random() * 100000));
-    // });
-    //
-    every((4).second(), function() {
-      my.sphero.roll(180, Math.floor(Math.random() * 360));
+    every((1).second(), function() {
+      my.sphero.setRGB(Math.floor(Math.random() * 100000));
     });
+
+    // every((4).second(), function() {
+    //   my.sphero.roll(180, Math.floor(Math.random() * 360));
+    // });
   }
 }).start();
 
